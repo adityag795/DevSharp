687b7af2419804d93e5b98a62944c44b6a5b625f
adityag795 <adityag795@gmail.com>
2017-10-07 21:39:40 +0530

    Single Linked List

--- LinkedList/SingleLinkedList/LinkedList.cs
+++ LinkedList/SingleLinkedList/LinkedList.cs
@@ -28,6 +28,42 @@ namespace SingleLinkedList
             System.Console.WriteLine();
         }
 
+        public void CountNodes()
+        {
+            int count = 0;
+            Node p = start;
+            while(p != null)
+            {
+                count++;
+                p = p.link;
+            }
+
+            System.Console.WriteLine("Number of nodes : " + count);
+        }
+
+        public bool Search(int x)
+        {
+            int position = 1;
+            Node p = start;
+            while(p != null)
+            {
+                if (p.info == x)
+                    break;
+                position++;
+                p = p.link;
+            }
+            if (p == null)
+            {
+                System.Console.WriteLine(x + " is not present in list.");
+                return false;
+            }
+            else
+            {
+                System.Console.WriteLine(x + " is at position " + position);
+                return true;
+            }
+        }
+
         
     }
 }
--- LinkedList/SingleLinkedList/Program.cs
+++ LinkedList/SingleLinkedList/Program.cs
@@ -10,28 +10,28 @@ namespace SingleLinkedList
 
             LinkedList list = new LinkedList();
 
+            Console.WriteLine("1. Display list.");
+            Console.WriteLine("2. Count the number of nodes.");
+            Console.WriteLine("3. Search for an element.");
+            Console.WriteLine("4. Insert in empty list / beginning of list.");
+            Console.WriteLine("5. Insert the node at the end of the list.");
+            Console.WriteLine("6. Insert a node after a specified node.");
+            Console.WriteLine("7. Insert a node before a specified node.");
+            Console.WriteLine("8. Insert a node at a given position.");
+            Console.WriteLine("9. Delete first node.");
+            Console.WriteLine("10. Delete last node.");
+            Console.WriteLine("11. Delete any node.");
+            Console.WriteLine("12. Reverse the list.");
+            Console.WriteLine("13. Bubble sort by exchanging DATA.");
+            Console.WriteLine("14. Bubble sort by exchanging LINKS.");
+            Console.WriteLine("15. Merge Sort.");
+            Console.WriteLine("16. Insert Cycle.");
+            Console.WriteLine("17. Detect Cycle.");
+            Console.WriteLine("18. Remove Cycle.");
+            Console.WriteLine("19. Quit");
+
             while (true)
             {
-                Console.WriteLine("1. Display list.");
-                Console.WriteLine("2. Count the number of nodes.");
-                Console.WriteLine("3. Search for an element.");
-                Console.WriteLine("4. Insert in empty list / beginning of list.");
-                Console.WriteLine("5. Insert the node at the end of the list.");
-                Console.WriteLine("6. Insert a node after a specified node.");
-                Console.WriteLine("7. Insert a node before a specified node.");
-                Console.WriteLine("8. Insert a node at a given position.");
-                Console.WriteLine("9. Delete first node.");
-                Console.WriteLine("10. Delete last node.");
-                Console.WriteLine("11. Delete any node.");
-                Console.WriteLine("12. Reverse the list.");
-                Console.WriteLine("13. Bubble sort by exchanging DATA.");
-                Console.WriteLine("14. Bubble sort by exchanging LINKS.");
-                Console.WriteLine("15. Merge Sort.");
-                Console.WriteLine("16. Insert Cycle.");
-                Console.WriteLine("17. Detect Cycle.");
-                Console.WriteLine("18. Remove Cycle.");
-                Console.WriteLine("19. Quit");
-
                 Console.WriteLine("\nEnter your choice: ");
                 choice = Convert.ToInt32(Console.ReadLine());
 
@@ -41,10 +41,21 @@ namespace SingleLinkedList
                 switch(choice)
                 {
                     case 1:
-
+                        list.DisplayList();
                         break;
                     case 2:
+                        list.CountNodes();
+                        break;
+                    case 3: 
+                        Console.WriteLine("Enter the element to be searched: ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.Search(data);
+                        break;
+                    // case 4:
 
+                    //     break;
+                    default: 
+                        Console.WriteLine("Please enter right choice.");
                         break;
                 }
                 Console.WriteLine();
